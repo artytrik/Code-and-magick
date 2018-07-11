0e3e6550520dfbda9446f65d35c2e781e74dc2f3
artytrik <tema-luch@mail.ru>
2018-07-12 03:19:09 +1000

    Добавил обработку ошибки при отправке формы

--- js/setup.js
+++ js/setup.js
@@ -98,14 +98,15 @@
     fireballColorInput.value = randomFireballColor;
   });
 
+  var onSumbitSuccess = function () {
+    userDialog.classList.add('hidden');
+  };
+
   var form = userDialog.querySelector('.setup-wizard-form');
   form.addEventListener('submit', function (evt) {
-    window.backend.save(new FormData(form), function () {
-      userDialog.classList.add('hidden');
-    });
+    window.backend.save(new FormData(form), onSumbitSuccess, errorHandler);
     evt.preventDefault();
   });
-
   window.backend.load(successHandler, errorHandler);
 })();
 
